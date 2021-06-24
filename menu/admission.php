06f28f68568dafcdcde979a114a6bb9d8a59944e
ansibtri <ansibtri961@gmail.com>
2021-06-24 18:32:16 +0545

    building admission system

--- menu/admission.php
+++ menu/admission.php
@@ -1,5 +1,29 @@
+<?php
+    
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        include '../partials/_dbconnect.php';
+        $fname = $_POST['fname'];
+        $lname = $_POST['surname'];
+        $dob = $_POST['dob'];
+        $class=$_POST['class'];
+        $caddress=$_POST['clocation'];
+        $plocation=$_POST['plocation'];
+        $section = $_POST['section'];
+        $phone=$_POST['phone'];
+        echo $fname."<br>".$lname."<br>".$dob."<br>".$class."<br>".$caddress."<br>".$plocation."<br>".$section."<br>".$phone;
+
+    }
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,19 +33,65 @@
     <link rel="stylesheet" href="../utilities.css">
     <link rel="stylesheet" href="admission.css">
 </head>
+
 <body>
     <?php include '../partials/_nav.php';?>
-    
-    <div class="container m-2">
-        <h1 class="text-center p-1">!!! Admission Open !!!</h1>
-        <div class="result-container m-4">
-            <h1 class="text-center p-1"> <img src="https://www.freelogoservices.com/api/main/images/1j+ojFVDOMkX9Wytexe43D6khvWAqR9PkRrNwXs1M3EMoAJtliIpgvFr9...4z" alt="" srcset=""></h1>
-            <div class="scl-add">
-            <p class="text-center bold">Galyang, Syangja</p>
-            <p class="text-center bold">Estd.2042</p>
+    <div class="container m-2 p-2">
+        <form action="admission.php" method="post">
+            <div class="detail grid grid-2 stuid">
+                <label for="studentid">Student ID:</label>
+                <input required type="text" name="studentid" id="student_id">
             </div>
-        </div>
+            <div class="detail grid grid-2 grid-gap p-1">
+                <div class="firstname grid-2 grid  grid-gap m-1">
+                    <label for="fname">FirstName:</label>
+                    <input required type="text" name="fname" id="fname" placeholder="Enter Firstname">
+                </div>
+                <div class="lastname grid-2 grid grid-gap m-1">
+                    <label for="surname">Lastname:</label>
+                    <input required type="text" name="surname" id="surname" placeholder="Enter Lastname">
+                </div>
+            </div>
+            <div class="detail grid grid-2 px-1">
+                <div class="dob grid-2 grid grid-gap m-1">
+                    <label for="dob">Date of Birth:</label>
+                    <input required type="date" name="dob" id="dob">
+                </div>
+                <div class="grade grid-2 grid grid-gap m-1">
+                    <label for="class">Class:</label>
+                    <input required type="number" name="class" id="class" placeholder="Class">
+                </div>
+            </div>
+            <div class="detail grid grid-2">
+                <div class="currentlocation grid-2 grid m-1 grid-gap">
+                    <label for="clocation">Current Address:</label>
+                    <input required type="text" name="clocation" id="clocation">
+                </div>
+                <div class="permlocation grid-2 grid grid-gap m-1">
+                    <label for="plocation">Permanent Address:</label>
+                    <input required type="text" name="plocation" id="plocation">
+                </div>
+            </div>
+            <div class="detail grid grid-2">
+                <div class="classsection grid-2 grid grid-gap m-1">
+                    <label for="Section">Section:</label>
+                    <select name="section" id="section">
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                    </select>
+                </div>
+                <div class="stucontact grid-2 grid grid-gap m-1">
+                    <label for="phone">Phone no:</label>
+                    <input required type="tel" name="phone" id="phone">
+                </div>
+            </div>
+            <div class="detail grid grid-2 grid-gap m-1">
+                <input type="reset" value="Reset">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
     </div>
     <?php include '../partials/_footer.php' ?>
 </body>
+
 </html>
